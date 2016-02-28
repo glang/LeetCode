f78bd0257897f8d9001a09bec4ed4c353df83972
Granger Lang <grangerlang8@hotmail.com>
2016-02-28 00:38:39 -0800

    passed the tests

--- Two Sum/TwoSum.cpp
+++ Two Sum/TwoSum.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    static vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> sorted = nums;
         sort(sorted.begin(), sorted.end());
         
-        for (int i = 0; i < sorted.size() - 1 && sorted.at(i) < target; ++i) {
-            if (binary_search(sorted.begin() + i + 1, sorted.end() , target - sorted.at(i))) {
+        for (int i = 0; i < sorted.size() - 1; ++i) {
+            int toFind = target - sorted.at(i);
+
+            if (binary_search(sorted.begin() + i + 1, sorted.end() , toFind)) {
                 auto val1Iter = find(nums.begin(), nums.end(), sorted.at(i));
-                auto val2Iter = find(nums.begin(), nums.end(), target - sorted.at(i));
+                decltype(val1Iter) val2Iter;
+                
+                if (toFind == sorted.at(i))
+                    val2Iter = find(val1Iter + 1, nums.end(), toFind);
+                else
+                    val2Iter = find(nums.begin(), nums.end(), toFind);
+
                 vector<int> ret{static_cast<int>(val1Iter - nums.begin()), static_cast<int>(val2Iter - nums.begin())}; 
                 return ret;
             }
         }
     }
-
 };
