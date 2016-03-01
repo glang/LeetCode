20d655b79e5da9bcb4406078819751ea3b2de8d3
Granger Lang <grangerlang8@hotmail.com>
2016-02-29 21:10:48 -0800

    made a grave error of assuming a pointer * applied to all variables in a list declaration

--- Add Two Numbers/AddTwoNumbers.cpp
+++ Add Two Numbers/AddTwoNumbers.cpp
@@ -8,26 +8,45 @@
  */
 class Solution {
 public:
-	void reverse(ListNode* nodePtr, ListNode* newHead&) {
+	void reverse(ListNode* nodePtr, ListNode*& newHead) {
 		if (nodePtr->next == NULL) {
 			newHead = nodePtr;
 			return;
 		}
 
-		reverse(nodePtr->next);
+		reverse(nodePtr->next, newHead);
 		nodePtr->next->next = nodePtr;
 	}
 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		ListNode* rl1, rl2;
+		ListNode *rl1, *rl2, *ret, *temp = ret;
 
 		reverse(l1, rl1); // l1 is new head of reversed list
-		l1->next = NULL; //setting the end of linked list to NULL
+		l1->next = NULL; // setting the end of linked list (former head) to NULL
 		reverse(l2, rl2);
 		l2->next = NULL;
 
-		return 0;
+		int carry = 0;
+
+		while (rl1 && rl2) {
+			int digit = rl1->next->val + rl2->next->val + carry;
+
+			if (digit > 10) {
+				digit -= 10;
+				carry = 1;
+			} else {
+				carry = 0;
+			}
+
+			cout << digit << endl;
+
+			ListNode cur = ListNode(digit);
+			temp = &cur;
+			temp = temp->next;
+			rl1 = rl1->next;
+			rl2 = rl2->next;
+		}
+
+		return ret;
     }
 };
-
-// [] -> [] -> [] ->
