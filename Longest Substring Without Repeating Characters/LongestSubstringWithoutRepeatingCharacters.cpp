f4da6c64d6143262e7cbf3dd80d3df3cf8ee424a
Granger Lang <grangerlang8@hotmail.com>
2016-03-03 00:44:50 -0800

    getting close, but I'm brainfried right now

--- Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cpp
+++ Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int max = 0;
+        int max = 0, cur = 0;
         map<char, int> track;
         
-        for (int i = 0; i !+ s.size(); ++i) {
-            
+        for (int i = 0; i != s.size(); ++i) {
+        	auto iter = track.find(s.at(i));
+            if (iter == track.end()) {
+            	cur++;
+            	track.emplace(s.at(i), i);
+            } else { // consider value of cur and index distance from last occurance
+            	if (cur > max)
+            		max = cur;
+            	int distance = i - track.at(s.at(i));
+            	cur = distance < cur ? cur + 1 : distance;
+            	track[s.at(i)] = i;
+            }
         }
+
+    	if (cur > max)
+    		max = cur;
+
+        return max;
     }
 };
