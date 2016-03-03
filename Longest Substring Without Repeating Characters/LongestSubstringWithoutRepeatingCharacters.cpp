2f208f37f398a0ca0d8f8f9978483accd1e313d1
Granger Lang <grangerlang8@hotmail.com>
2016-03-02 23:35:55 -0800

    brainstorming a non-(O(N^2)) solution

---
+++ Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int max = 0;
+        map<char, int> track;
+        
+        for (int i = 0; i !+ s.size(); ++i) {
+            
+        }
+    }
+};
