b0d74cbfdca5f612a151b526d6975c7130a9478e
Granger Lang <grangerlang8@hotmail.com>
2016-03-07 13:48:26 -0800

    this problem is easily one of the most annoying problems I have ever attempted. The corner cases are limitless

--- String to Integer/StringToInteger.cpp
+++ String to Integer/StringToInteger.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-	void isValid(string &str) {
-		for (int i = 0; i < str.size(); ++i) {
-			char c = str.at(i);
-
-			if (i == 1 && str.size() > 1 && (c == '-' || c == '+'))
-				continue;
-
-			if (!(c >= 'a' && i <= 'z') || (c >= 'A' && c <= 'Z'))) {
-				return false;	
-		}
-
-		return true;
-	}
-
     int myAtoi(string str) {
     	auto firstNonWS = str.find_first_not_of(' ');
-    	ret = str.substr(firstNonWS, str.size() - firstNonWS);
-        
-    	if (!isValid(ret))
-    		return 0;
-    	
 
+    	if (firstNonWS == -1) return 0;
+
+    	string ret = str.substr(firstNonWS, str.size() - firstNonWS);
+     
+		for (int i = 0; i < ret.size(); ++i) {
+			char c = ret.at(i);
+
+			if (i == 0 && (c == '-' || c == '+') && ret.size() > 1) continue;
+			if (c < '0' || c > '9') return 0;
+		}
+  
+		if (ret == "") return 0;
+
+		if (ret.at(0) == '-' && ret.compare("-9223372036854775807") > 1) return INT_MIN;
+		else if (ret.at(0) == '+' && ret.compare("+9223372036854775807") > 1) return INT_MAX;
+		else if (ret.compare("9223372036854775807") > 1) return INT_MAX;
+		
+		long long l = stoll(ret);
+
+		if (l < INT_MIN) return INT_MIN;
+		if (l > INT_MAX) return INT_MAX;
+
+		return stoi(ret);
     }
 };
-
-//overflow
-//negatives
-//0s
-//invalid strings? spaces, other symbols, etc
