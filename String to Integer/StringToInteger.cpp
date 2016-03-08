26e97a0125f4b941bfd6697505a1586621e1220d
Granger Lang <grangerlang8@hotmail.com>
2016-03-07 21:17:55 -0800

    finally

--- String to Integer/StringToInteger.cpp
+++ String to Integer/StringToInteger.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     int myAtoi(string str) {
-    	auto firstNonWS = str.find_first_not_of(' ');
+    	int firstNonWS = str.find_first_not_of(' ');
+    	
+    	if (firstNonWS == string::npos) return 0;
+    	
+    	string trim = str.substr(firstNonWS, str.size() - firstNonWS);
+    	long long res = 0, sign = 1;
+    	
+		for (int i = 0; i < trim.size(); ++i) {
+			char c = trim.at(i);
 
-    	if (firstNonWS == -1) return 0;
+			if (i == 0 && (c == '-' || c == '+')) {
+			    if (trim.size() == 1) return 0;
+				if (c == '-')
+					sign = -1;
+				continue;	
+			}
 
-    	string ret = str.substr(firstNonWS, str.size() - firstNonWS);
-     
-		for (int i = 0; i < ret.size(); ++i) {
-			char c = ret.at(i);
+			if (c >= '0' && c <= '9') {
+				res = res * 10 + c - '0';
 
-			if (i == 0 && (c == '-' || c == '+') && ret.size() > 1) continue;
-			if (c < '0' || c > '9') return 0;
+				if (res * sign >= INT_MAX) return INT_MAX;
+				if (res * sign <= INT_MIN) return INT_MIN;
+			} else {
+				break;
+			}
 		}
   
-		if (ret == "") return 0;
-
-		if (ret.at(0) == '-' && (ret.size() > 20 || ret.compare("-9223372036854775807") > 1)) return INT_MIN;
-		else if (ret.at(0) == '+' && (ret.size() > 20 || ret.compare("+9223372036854775807") > 1)) return INT_MAX;
-		else if (ret.size() > 19 || ret.compare("9223372036854775807") > 1) return INT_MAX;
-		
-		long long l = stoll(ret);
-
-		if (l < INT_MIN) return INT_MIN;
-		if (l > INT_MAX) return INT_MAX;
-
-		return stoi(ret);
+		return sign * res;
     }
 };
