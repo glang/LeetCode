67f9f9d6832c9fbe3b2b6261faa47279ccc41942
Granger Lang <grangerlang8@hotmail.com>
2016-03-10 10:12:07 -0800

    implemented a basic case

--- Regular Expression Matching/RegularExpressionMatching.cpp
+++ Regular Expression Matching/RegularExpressionMatching.cpp
@@ -1,6 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        
-    }
+   static bool isMatch(string s, string p) {
+      char sidx = 0, pidx = 0;
+
+      if (p.at(pidx) == '.' || 
+         (((pidx + 1 < p.size() && p.at(pidx + 1) != '*')
+          || pidx == p.size() - 1) && s.at(sidx) == p.at(pidx))) {
+         sidx++;
+         pidx++;
+      } else {
+         return false;
+      }
+
+      return true;
+   }
 };
+
+int main() {
+   if (Solution::isMatch("test", "tesg")) {
+      cout << "match" << endl;
+   } else {
+      cout << "not match" << endl;
+   }
+
+   return 0;
+}
