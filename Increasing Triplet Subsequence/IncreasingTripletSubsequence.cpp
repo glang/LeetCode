938f6a7ee674fc43a748f4dc17ba8f4ea3952fbd
Granger Lang <grangerlang8@hotmail.com>
2016-03-13 19:59:17 -0700

    retry

---
+++ Increasing Triplet Subsequence/IncreasingTripletSubsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        if (nums.size() < 3) return false;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            
+        }
+        
+        return false;
+    }
+};
