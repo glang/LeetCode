9a2e1c568cae381bb57e6b19c1b5963e2e6b0264
Granger Lang <grangerlang8@hotmail.com>
2016-03-14 16:03:49 -0700

    peeked at clever solution

--- Increasing Triplet Subsequence/IncreasingTripletSubsequence.cpp
+++ Increasing Triplet Subsequence/IncreasingTripletSubsequence.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        if (nums.size() < 3) return false;
-        
+        int n1 = INT_MAX, n2 = INT_MAX;
+
         for (int i = 0; i < nums.size(); ++i) {
-            
+            int cur = nums.at(i);
+            if (cur <= n1)
+            	n1 = cur;
+            else if (cur <= n2)
+            	n2 = cur;
+            else
+            	return true;
         }
         
         return false;
     }
 };
+
+// [1, -2, 3, -2, 5]
+// [2, 4, -2, -3]
+// [4, 5, 1, 0, 2, 3]
+// [2, 5, 1, -5, 6]
+// 4, -2, 7, -3, 9 
