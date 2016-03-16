d08d03835bad4cd455f40f48b0cf42d16d1573f6
Granger Lang <grangerlang8@hotmail.com>
2016-03-16 00:18:47 -0700

    passed 21/22 tests -.-

--- Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
+++ Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
@@ -4,19 +4,37 @@ public:
     	if (nums.empty()) return 0;
         int max = 0;
         for (int i = 0; i < nums.size(); ++i) {
-        	int curNum = nums.at(i), curLength = 1;
+        	int min = nums.at(i), firstNum = min, firstLength = 1, secNum = min, secLength = 1;
         	for (int j = i; j < nums.size(); ++j) {
-        		if (nums.at(j) > curNum) {
-        			curNum = nums.at(j);
-        			++curLength;
+        		int curNum = nums.at(j);
+        		if (curNum > firstNum) {
+        			firstNum = curNum;
+        			++firstLength;
+        		}
+        		else {
+        			if (curNum > min && secLength == 1) {
+        				++secLength;
+        				secNum = curNum;
+        			} 
+        			else if (curNum > secNum) {
+        				++secLength;
+        				secNum = curNum;
+        			}
+	        		if (secLength > firstLength) {
+	        			firstLength = secLength;
+	        			firstNum = secNum;
+	        			secLength = 1;
+	        			secNum = min;
+	        		}
         		}
         	}
 
-        	max = std::max(max, curLength);
+        	max = std::max(max, firstLength);
         }
 
         return max;
     }
 };
 
-// [10,9,2,5,3,4]
+// [2,5,6,3,4,5]
+// [10,9,2,5,3,7,200,18,101,102,103]
