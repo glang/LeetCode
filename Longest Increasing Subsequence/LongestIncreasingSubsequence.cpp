00ca38af8afdb52bdbbe50dced144c1e880c39ca
Granger Lang <grangerlang8@hotmail.com>
2016-03-15 23:34:38 -0700

    passed 19/22 tests

--- Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
+++ Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
+    	if (nums.empty()) return 0;
+        int max = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+        	int curNum = nums.at(i), curLength = 1;
+        	for (int j = i; j < nums.size(); ++j) {
+        		if (nums.at(j) > curNum) {
+        			curNum = nums.at(j);
+        			++curLength;
+        		}
+        	}
+
+        	max = std::max(max, curLength);
+        }
+
+        return max;
     }
 };
+
+// [10,9,2,5,3,4]
