f1646a6c461f6ccaee819175f0ee07c0ccc9d451
Granger Lang <grangerlang8@hotmail.com>
2016-03-17 17:31:13 -0700

    the clever solution

--- Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
+++ Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-    	if (nums.empty()) return 0;
-        int max = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-        	int min = nums.at(i), firstNum = min, firstLength = 1, secNum = min, secLength = 1;
-        	for (int j = i; j < nums.size(); ++j) {
-        		int curNum = nums.at(j);
-        		if (curNum > firstNum) {
-        			firstNum = curNum;
-        			++firstLength;
-        		}
-        		else {
-        			if (curNum > min && secLength == 1) {
-        				++secLength;
-        				secNum = curNum;
-        			} 
-        			else if (curNum > secNum) {
-        				++secLength;
-        				secNum = curNum;
-        			}
-	        		if (secLength > firstLength) {
-	        			firstLength = secLength;
-	        			firstNum = secNum;
-	        			secLength = 1;
-	        			secNum = min;
-	        		}
-        		}
-        	}
-
-        	max = std::max(max, firstLength);
+        vector<int> res;
+        for(int i=0; i<nums.size(); i++) {
+            auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+            if(it==res.end()) res.push_back(nums[i]);
+            else *it = nums[i];
         }
-
-        return max;
+        return res.size();
     }
 };
 
-// [2,5,6,3,4,5]
 // [10,9,2,5,3,7,200,18,101,102,103]
+
+/*
+10
+9
+2
+2,5
+2,3
+2,3,7
+2,3,7,200
+2,3,7,18
+2,3,7,28,101
+2,3,7,28,101,102
+2,3,7,28,101,102, 103
+*/
