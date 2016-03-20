4b4711f5bf989518d3d8627d8a6dc37a341e6871
Granger Lang <grangerlang8@hotmail.com>
2016-03-19 22:29:04 -0700

    adding better personal version

--- Longest Palindromic Substring/LongestPalindromicSubstring.cpp
+++ Longest Palindromic Substring/LongestPalindromicSubstring.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-    	int maxLen = 0, loc = 0;
+    	int maxLen = 1, loc = 0;
 
         for (int i = 0; i < s.size(); ++i) {
+        	 // odd character palindrome case
         	for (int j = 0; i - j >= 0 && i + j < s.size() && s.at(i - j) == s.at(i + j); ++j) {
 	        	if (maxLen < j * 2 + 1) {
 	        		maxLen = j * 2 + 1;
 	        		loc = i - j;
 	        	}
 			}
-        }
 
-        for (int i = 0; i < s.size(); ++i) {
+			 // even character palindrome case
         	for (int j = 1; i - j + 1 >= 0 && i + j < s.size() && s.at(i - j + 1) == s.at(i + j); ++j) {
 	        	if (maxLen < j * 2) {
 	        		maxLen = j * 2;
