964e0774d62d6349dd0637f3eb3f9726a7f26ceb
Granger Lang <grangerlang8@hotmail.com>
2016-03-21 17:58:49 -0700

    changed the intial values to -1 instead of INT_MAX

--- Coin Change/CoinChange.cpp
+++ Coin Change/CoinChange.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> min(amount + 1, numeric_limits<int>::max());
+        vector<int> min(amount + 1, -1);
    		min.at(0) = 0;
 
    		for (int i = 1; i <= amount; ++i) {
    			for (int j = 0; j < coins.size(); ++j) {
-   				if (i >= coins.at(j) && min.at(i - coins.at(j)) + 1 < min.at(i))
-   					min.at(i) = min.at(i - coins.at(j)) + 1;
+   				int oldVal = min[i - coins[j]];
+   				if (i >= coins[j] && oldVal != -1 && (min[i] == -1 || oldVal + 1 < min[i]))
+   					min[i] = oldVal + 1;
    			}
    		}
 
-   		return min.at(amount);
+   		if (min[amount] == -1)
+   			return -1;
+   		return min[amount];
     }
 };
+
+/*
+
+coins = {2, 5, 10, 25}
+
+S = 7
+
+      [0,  1, 2,  3,  4,  5,  6,  7]
+min = [0, -1, 1, -1, -1, -1, -1, -1]
+
+*/
