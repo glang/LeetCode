0ba9134ae89f31860e7720dc3c7f78c6413d79d5
Granger Lang <grangerlang8@hotmail.com>
2016-03-31 00:49:08 -0700

    added ascii illustration comment

--- Rectangle Area/source.cpp
+++ Rectangle Area/source.cpp
@@ -1,6 +1,34 @@
 class Solution {
-public:
-    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        
+public:	
+    int computeArea(int leftX1, int leftY1, int rightX1, int rightY1, int leftX2, int leftY2, int rightX2, int rightY2) {
+    	int res = 0;
+    	int overlapWidth = -1, overlapHeight = -1;
+
+    	if (leftX1 == leftX2 && rightX1 == rightX1) {
+    		overlapWidth = rightX1 - leftX1;
+    	}
+
+    	if (leftY1 == leftY2 && rightY1 == leftY2) {
+    		overlapHeight = rightY1 - leftY1;
+    	}
+
+    	if (overlapHeight == 0 || overlapWidth == 0) {
+    		return 0;
+    	}
+
+
     }
 };
+
+/*
+				CD
+ ---------------- 
+ | 				 |
+ |				 |        GH
+ |			----- -------
+ |			|	 |		|
+ ----------------		|
+ AB			|       	|
+ 			 -----------
+ 		  EF	 	
+*/
