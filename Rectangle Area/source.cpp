9afad0d1ce3c56a020d10f1ce93a763be836f339
Granger Lang <grangerlang8@hotmail.com>
2016-03-31 16:15:43 -0700

    implemented test for no overlap, and one case of x overlap

--- Rectangle Area/source.cpp
+++ Rectangle Area/source.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:	
-    int computeArea(int leftX1, int leftY1, int rightX1, int rightY1, int leftX2, int leftY2, int rightX2, int rightY2) {
+    int computeArea(int min_x1, int min_y1, int max_x1, int max_y1, int min_x2, int min_y2, int max_x2, int max_y2) {
     	int res = 0;
-    	int overlapWidth = -1, overlapHeight = -1;
+    	int overlapWidth = 0, overlapHeight = 0;
 
-    	if (leftX1 == leftX2 && rightX1 == rightX1) {
-    		overlapWidth = rightX1 - leftX1;
-    	}
-
-    	if (leftY1 == leftY2 && rightY1 == leftY2) {
-    		overlapHeight = rightY1 - leftY1;
-    	}
-
-    	if (overlapHeight == 0 || overlapWidth == 0) {
+    	//check if no overlap
+    	if (max_x1 <= min_x2 || max_x2 <= min_x1 || max_y1 <= min_y2 || max_y2 <= min_y1) {
     		return 0;
     	}
 
-
+    	//find width of overlap
+    	if (xmin2 >= xmin1) {
+    		if (xmax2 >= xmax1) {
+    			overlapWidth = xmax1 - xmin2;
+    		} else {
+    			overlapWidth = xmax2 - xmin2;
+    		}
+    	}
     }
 };
 
