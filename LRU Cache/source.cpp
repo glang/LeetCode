092a14409feabb8a0ad4b14063b987a30ac79cfa
Granger Lang <grangerlang8@hotmail.com>
2016-04-02 20:07:11 -0700

    wrote the get function

--- LRU Cache/source.cpp
+++ LRU Cache/source.cpp
@@ -1,14 +1,31 @@
+#include <map>
+#include <vector>
+
+using namespace std;
+
 class LRUCache{
 public:
     LRUCache(int capacity) {
-        
+    	maxSize = capacity;
     }
     
     int get(int key) {
-        
+        if (cache.find(key) == cache.end()) {
+        	return -1;
+        } else {
+        	cache[key][1]++;
+        	return cache[key][0]);
+        }
     }
     
     void set(int key, int value) {
-        
+    	if (cache.size() < maxSize) {
+    		cache[key] = value;
+    	} else {
+
+    	}
     }
+private:
+	int maxSize;
+	map<int, vector<int>> cache;
 };
