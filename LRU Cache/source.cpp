b72a4c5fc4dba51b19bf0006a1c5a22fea09e194
Granger Lang <grangerlang8@hotmail.com>
2016-04-03 12:38:40 -0700

    finished set function; working on get function

--- LRU Cache/source.cpp
+++ LRU Cache/source.cpp
@@ -1,5 +1,6 @@
 #include <map>
-#include <vector>
+#include <list>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,19 +14,24 @@ public:
         if (cache.find(key) == cache.end()) {
         	return -1;
         } else {
+        	int temp = find(order.begin(), order.end(), key);
+
         	return cache[key];
         }
     }
     
     void set(int key, int value) {
-    	if (cache.size() < maxSize) {
-    		cache[key] = value;
-    	} else {
-
+    	if (cache.size() >= maxSize) {
+    		cache.erase(cache.find(order.back()));
+    		order.pop_back();
     	}
+
+		cache[key] = value;
+		order.push_front(key);
     }
 
 private:
 	int maxSize;
 	map<int, int> cache;
+	list<int> order;
 };
