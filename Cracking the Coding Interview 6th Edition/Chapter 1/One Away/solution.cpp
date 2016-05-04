089c0b60a13f465380bc2ddd2936166e5fe5dcf4
Granger Lang <grangerlang8@hotmail.com>
2016-05-04 16:45:46 -0700

    make it One Away problem more concise and removed sorting

--- Cracking the Coding Interview 6th Edition/Chapter 1/One Away/solution.cpp
+++ Cracking the Coding Interview 6th Edition/Chapter 1/One Away/solution.cpp
@@ -1,56 +1,33 @@
-//remove, insert, replace
 bool oneAway(string before, string after) {
 	bool edited = false;
-
-	sort(before.begin(), begin.end());
-	sort(after.begin(), after.end());
 
 	auto beforeIter = before.begin();
 	auto afterIter = after.begin();
 
-	if (after.size() - 1 == before.size()) {
-		while (beforeIter != before.end() && afterIter != after.end()) {
-			if (*beforeIter != *afterIter) {
-				if (edited) {
-					return false;
-				}
-
-				isEdited = true;
-				++beforeIter;
-			} else {
-				++beforeIter;
-				++afterIter;
-			}
-		}
-	} else if (after.size() + 1 == before.size()) {
-		while (beforeIter != before.end() && afterIter != after.end()) {
-			if (*beforeIter != *afterIter) {
-				if (edited) {
-					return false;
-				}
-
-				isEdited = true;
-				++afterIter;
-			} else {
-				++afterIter;
-				++beforeIter;
-			}
-		}
-	} else if (after.size() == before.size()) {
-		while (beforeIter != before.end() && afterIter != after.end()) {
-			if (*beforeIter != *afterIter) {
-				if (edited) {
-					return false;
-				}
-
-				isEdited = true;
-			}
-			
-			++afterIter;
-			++beforeIter;
-		}
-	} else {
+	if (after.size() < before.size() - 1 || after.size() > before.size() + 1) {
 		return false;
+	}
+
+	while (beforeIter != before.end() && afterIter != after.end()) {
+		if (*beforeIter != *afterIter) {
+			if (edited) {
+				return false;
+			}
+
+			edited = true;
+
+			if (after.size() - 1 == before.size()) {
+				++beforeIter;
+			} else if (after.size() + 1 == before.size()) {
+				++afterIter;
+			} else {
+				++beforeIter;
+				++afterIter;
+			}
+		} else {
+			++beforeIter;
+			++afterIter;
+		}
 	}
 
 	return true;
