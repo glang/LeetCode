211ac93b21442b506a6ce4e514a62ae4b8db2184
Granger Lang <grangerlang8@hotmail.com>
2016-05-05 22:07:25 -0700

    wrote the more efficient solution to the delete middle node problem

--- Cracking the Coding Interview 6th Edition/Chapter 2: Linked Lists/Delete Middle Node/solution.cpp
+++ Cracking the Coding Interview 6th Edition/Chapter 2: Linked Lists/Delete Middle Node/solution.cpp
@@ -1,10 +1,5 @@
-//shift all Node data toward the head via copying
+//copy data from next node, then delete next node
 void deleteMiddleNode(shared_ptr<Node> node) {
-	shared_ptr iter = node;
-	while (iter->next->next) {
-		iter->val = iter->next->val;
-		iter = iter->next;
-	}
-
-	iter->next = 0;
+	node->val = node->next->val;
+	node->next = node->next->next;
 }
