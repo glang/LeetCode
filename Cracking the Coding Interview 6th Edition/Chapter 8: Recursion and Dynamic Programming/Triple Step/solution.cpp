2be8bfb1ad6aba0b1cd518c588d96fd9f8c73a38
Granger Lang <grangerlang8@hotmail.com>
2016-05-06 01:49:27 -0700

    brainstorming the triple step problem. Will get back to it in the morning

---
+++ Cracking the Coding Interview 6th Edition/Chapter 8: Recursion and Dynamic Programming/Triple Step/solution.cpp
@@ -0,0 +1,9 @@
+//A child can take 1, 2, or 3 steps, how many ways can the child climb n steps?
+
+void tripleStep(int n, int& ways, int stepInc, int stepCount) {
+	if (stepCount + )
+}
+
+int main() {
+	
+}
