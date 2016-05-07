b52c74a6964bb60760761f3607da46ba8f4aae56
Granger Lang <grangerlang8@hotmail.com>
2016-05-07 10:57:19 -0700

    still trying to get the binary search right for edge case

--- Programming Interviews Exposed 3rd Edition/Chapter 7: Recursion/Binary Search/solution.cpp
+++ Programming Interviews Exposed 3rd Edition/Chapter 7: Recursion/Binary Search/solution.cpp
@@ -16,28 +16,31 @@ int bsearch(int key, int* array, int size, int startIndex) {
 	} else if (key < midVal) {
 		return bsearch(key, array, size/2, startIndex);
 	} else {
-		return bsearch(key, array, size/2, startIndex + size/2 + 1);
+		int isOdd = size%2 == 1 ? 1 : 0;
+		return bsearch(key, array, size/2 + isOdd, startIndex + size/2);
 	}
 }
 
 int main() {
+/*
 	int arr[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	cout << bsearch(3, arr, 9, 0) << endl;
 	cout << bsearch(4, arr, 9, 0) << endl;
 	cout << bsearch(5, arr, 9, 0) << endl;
 	cout << bsearch(1, arr, 9, 0) << endl;
 	cout << bsearch(9, arr, 9, 0) << endl;
-/*
+*/
 	int arr[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 	cout << bsearch(3, arr, 8, 0) << endl;
 	cout << bsearch(4, arr, 8, 0) << endl;
 	cout << bsearch(5, arr, 8, 0) << endl;
 	cout << bsearch(1, arr, 8, 0) << endl;
 	cout << bsearch(8, arr, 8, 0) << endl;
-*/
 }	
 
 /*
+[1 2 3 4] 5 [6 7 8 9]
+
 bs(9, arr, 9, 0)
 	midVal = arr[4] = 5
 	bs(9, arr, 4, 5)
