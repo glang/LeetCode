e82f3bfdb17f80fd53da6cbca30e5295a5b23c3a
Granger Lang <grangerlang8@hotmail.com>
2016-05-14 14:54:28 -0700

    added resursive solution for power set problem

--- Cracking the Coding Interview 6th Edition/Chapter 8: Recursion and Dynamic Programming/Power Set/solution.cpp
+++ Cracking the Coding Interview 6th Edition/Chapter 8: Recursion and Dynamic Programming/Power Set/solution.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 vector<vector<int>> powerSet(vector<int> origSet);
 void powerSet(vector<vector<int>>& allSets, int newItem);
+vector<vector<int>> powerSetRecurse(vector<int>& origSet);
+vector<vector<int>> powerSetRecurse(vector<int>& origSet, vector<vector<int>>& allSets, int index);
 
 vector<vector<int>> powerSet(vector<int> origSet) {
    vector<vector<int>> allSets({vector<int>()});
@@ -26,12 +28,43 @@ void powerSet(vector<vector<int>>& allSets, int newItem) {
    allSets.insert(allSets.end(), copy.begin(), copy.end());
 }
 
+vector<vector<int>> powerSetRecurse(vector<int>& origSet) {
+   vector<vector<int>> allSets = vector<vector<int>>({vector<int>()});
+   return powerSetRecurse(origSet, allSets, origSet.size() - 1);	
+}
+
+vector<vector<int>> powerSetRecurse(vector<int>& origSet, vector<vector<int>>& allSets, int index) {
+	if (index == -1) {
+		return vector<vector<int>>({vector<int>()});
+	}
+
+	vector<vector<int>> prevPowerSet = powerSetRecurse(origSet, allSets, index - 1);
+
+	for (vector<int>& innerSet : prevPowerSet) {
+		innerSet.push_back(origSet[index]);
+	}
+
+	allSets.insert(allSets.end(), prevPowerSet.begin(), prevPowerSet.end());
+
+	return allSets;
+}
+
 int main() {
    vector<int> origSet({1, 2, 3, 4});
 
-   vector<vector<int>> allSets = powerSet(origSet);
+   vector<vector<int>> allSetsIter = powerSet(origSet);
+   vector<vector<int>> allSetsRecurse = powerSetRecurse(origSet);
 
-   for (vector<int> singleSet : allSets) {
+   for (vector<int> singleSet : allSetsIter) {
+      for (int setItem : singleSet) {
+         cout << setItem << " ";
+      }
+      cout << endl;
+   }
+
+   cout << endl;
+
+   for (vector<int> singleSet : allSetsRecurse) {
       for (int setItem : singleSet) {
          cout << setItem << " ";
       }
