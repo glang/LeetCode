936dfaf0d2916e8c4c2b00449eeb35cabe5efb33
Granger Lang <grangerlang8@hotmail.com>
2016-05-17 23:29:12 -0700

    added iterative solution to List of Depths problem

--- Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/List of Depths/solution.cpp
+++ Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/List of Depths/solution.cpp
@@ -18,6 +18,33 @@ void insertNode(vector<list<shared_ptr<Node>>> &lists, shared_ptr<Node> node, in
 	insertNode(lists, node->right, depth + 1);
 }
 
+vector<list<shared_ptr<Node>>> listOfDepthsIter(shared_ptr<Node> root) {
+	vector<list<shared_ptr<Node>>>> lists;
+
+	list<shared_ptr<Node>> current;
+	current.push_back(root);
+	lists.push_back(current);
+
+	while (current.size() > 0) {
+		list<shared_ptr<Node>> next;
+
+		for (shared_ptr<Node> node : current) {
+			if (node->left != nullptr) {
+				next.push_back(node->left);
+			}
+
+			if (node->right != nullptr) {
+				next.push_back(node->right);
+			}
+		}
+
+		lists.push_back(next);
+		current = next;
+	}
+
+	return lists;
+}
+
 /*
         o
     o        o
