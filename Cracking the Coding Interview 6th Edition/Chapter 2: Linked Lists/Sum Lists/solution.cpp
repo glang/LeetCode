6d56f11fb6a59cfb06c56b2007e42bc8896ff935
Granger Lang <grangerlang8@hotmail.com>
2016-05-19 16:16:59 -0700

    added recursive solution to Sum Lists problem

--- Cracking the Coding Interview 6th Edition/Chapter 2: Linked Lists/Sum Lists/solution.cpp
+++ Cracking the Coding Interview 6th Edition/Chapter 2: Linked Lists/Sum Lists/solution.cpp
@@ -42,3 +42,61 @@ list<int> sumList(list<int> A, list<int> B) {
 1
 1 0 0 0
 */
+
+/*
+sum lists
+
+9 9 9
+2 1
+*/
+
+typedef shared_ptr<Node> sp;
+
+void sumListsRecurse(list<sp> result, sp Anode, sp Bnode, int prevcarry) {
+	if (Anode == nullptr && Bnode == nullptr && carry == 0) {
+		return;
+	}
+
+   int sum = prevcarry;
+
+   if (Anode != nullptr) {
+      sum += Anode->val;
+   }
+
+   if (Bnode != nullptr) {
+      sum += Bnode->val;
+   }
+
+   int carry = 0;
+
+   if (sum > 9) {
+   	carry = 1;
+   	sum = sum - 10;
+   }
+
+   result.push_back(Node(sum));
+   sumLists(result, Anode != nullptr ? Anode->next : nullptr, Bnode != nullptr ? Bnode->next : nullptr, carry);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
