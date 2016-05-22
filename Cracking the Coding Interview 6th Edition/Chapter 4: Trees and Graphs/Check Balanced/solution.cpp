aa46eed0b9358eb49088fbb253eec7f8d15d6138
Granger Lang <grangerlang8@hotmail.com>
2016-05-21 19:02:49 -0700

    added O(N) solution to Check Balanced problem

--- Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Check Balanced/solution.cpp
+++ Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Check Balanced/solution.cpp
@@ -1,9 +1,20 @@
+
+//O(NlogN) verison
+
 bool checkBalanced(shared_ptr<Node> root) {
    if (root == nullptr) {
       return true;
    }
 
-   return abs(treeHeight(root->left) - treeHeight(root->right)) > 1;
+   int leftTreeHeight = treeHeight(node->left);
+   int rightTreeHeight = treeHeight(node->right);
+
+   if (abs(leftTreeHeight - rightTreeHeight) > 1) {
+      return false;
+   } else {
+      return checkBalanced(root->left) && checkBalanced(root->right);
+   }
+   
 }
 
 int treeHeight(shared_ptr<Node> node) {
@@ -11,7 +22,32 @@ int treeHeight(shared_ptr<Node> node) {
       return 0;
    } 
 
-   return max(treeHeight(node->left), treeHeight(node->right));
+   return 1 + max(treeHeight(node->left), treeHeight(node->right));
+}
+
+///O(N) version
+
+bool checkBalanced(shared_ptr<Node> root) {
+   if (root == nullptr) {
+      return true;
+   }
+
+   return treeHeight(root->left) != INT_MIN && treeHeight(root->right) != INT_MIN;   
+}
+
+int treeHeight(shared_ptr<Node> node) {
+   if (node == nullptr) {
+      return 0;
+   } 
+
+   int leftTreeHeight = treeHeight(node->left);
+   int rightTreeHeight = treeHeight(node->right);
+
+   if (leftTreeHeight == INT_MIN || rightTreeHeight == INT_MIN || abs(leftTreeHeight - rightTreeHeight) > 1) {
+      return INT_MIN;
+   } else {
+      return 1 + max(treeHeight(node->left), treeHeight(node->right));
+   }
 }
 
 /*
