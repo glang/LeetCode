447f8d12fc5200cccebd2f41d2656baeecc03def
Granger Lang <grangerlang8@hotmail.com>
2016-05-23 17:35:40 -0700

    implemented most of the sort stack besides the inner sort stack

--- Cracking the Coding Interview 6th Edition/Chapter 3: Stacks and Queues/Sort Stack/solution.cpp
+++ Cracking the Coding Interview 6th Edition/Chapter 3: Stacks and Queues/Sort Stack/solution.cpp
@@ -1,38 +1,97 @@
 /*
-	sort a stack such that the smallest element is on top. Can use a temp stack.
+   sort a stack such that the smallest element is on top. Can use a temp stack.
 
-14 1 9 3 6
+5 4 9 3    6
 
 ---
-14 1 9 3
-6
+5 4 9      6
+3
 ---
-3 < 6
-14 1 9
-6 3
+5 4        9
+3 6
+---
+5          4
+3 6 9
+---
+5 9 6      4
+3
 --
-9 > 3 and 6
-put 9 in temp var
-push 3 and 6 back in old stack
-push 9 in holder
-
+5 9        6
+3 4
+--
+5          9
+3 4 6 
+--
+           5          
+3 4 6 9
+--
+9 6         5          
+3 4 
+--
 
 make the second stack sorted in reverse, then push all elements back to original stack
 
 */
 
 void sortStack(stack<int>& stack) {
-	stack<int> holder;
-	int temp;
+   stack<int> holder;
+   int numElements = stack.size();
+   int temp = stack.top();
+   stack.pop();
 
-	while (!stack.empty()) {
-		temp = stack.top();
-		if (holder.empty() || holder.top > temp) {
-			holder.push(stack.top());
-			stack.pop();
-		} else {
+   while (holder.size() != numElements) {
+      if (!stack.empty()) {
+         int top = stack.top();
+         if (top >= temp) {
+            holder.push(temp);
+            temp = top;
+            stack.pop();
+         } else {
+            if (holder.empty() || holder.top() < top) {
+               holder.push(top);
+            } else {
+               holder.push(temp);
+               temp = top;
 
-		}
-	}
-
+               //inner sort
+            }
+               
+            stack.pop();
+         }
+      } else {
+         holder.push(temp);
+      }
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
