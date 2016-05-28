8784415dd1e80015f7d07febeba3e84c753285d8
Granger Lang <grangerlang8@hotmail.com>
2016-05-27 20:49:40 -0700

    brainstorming BST successor problem

---
+++ Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Successor/solution.cpp
@@ -0,0 +1,20 @@
+/*
+
+       5
+   2       8
+  1 3     7 9
+     4   6
+
+*/
+
+Node* successor(Node* node) {
+	if (node->parent == null) {
+	
+	}
+
+	if (node->parent->data < node->data) {
+
+	} else {
+
+	}
+}
