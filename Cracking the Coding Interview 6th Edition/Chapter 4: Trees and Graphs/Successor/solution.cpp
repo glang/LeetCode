ef1b54160e7b7c4b68458aeb9d32533e18fa0502
Granger Lang <grangerlang8@hotmail.com>
2016-05-28 11:35:08 -0700

    wrote an iterative solution to successor problem

--- Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Successor/solution.cpp
+++ Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Successor/solution.cpp
@@ -5,13 +5,30 @@
   1 3     7 9
      4   6
 
+-might not have successor
+-might not have right node to find min in subtree
+-might be root and not have parent
+
 */
 
 Node* successor(Node* node) {
+   if (node->parent == nullptr && node->right == nullptr) {
+      return null;
+   }
+
 	if (node->right != null) {
 		return findMin(node->right);
 	} else {
-		
+		Node* up = findGreaterUp(node);
+
+      if (up == nullptr) {
+         return nullptr;
+      } else if (up->right == nullptr) {
+         return up;
+      } else {
+         Node* minNode = findMin(up->right);
+         return minNode->data < up->data ? minNode : up;      
+      }
 	}
 }
 
@@ -19,10 +36,20 @@ Node* findMin(Node* node) {
 	Node* min = node;
 	Node* iter = node;
 
-	while (iter != null) {
+	while (iter != nullptr) {
 		min = iter;
 		iter = iter->left;
 	}
 
 	return min;
+}
+
+Node* findGreaterUp(Node* node) {
+   Node* iter = node;
+
+   while (iter->data < node->data || node->parent != nullptr) {
+      iter = iter->parent;
+   }
+
+   return iter;
 }
