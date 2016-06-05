8009b19a32a1be2690d4ef650536cc30961547a0
Granger Lang <grangerlang8@hotmail.com>
2016-06-05 02:56:30 -0700

    added iter and recuse reverse linkedlist

--- Elements of Programming Interviews/Implement List Zipping/solution.cpp
+++ Elements of Programming Interviews/Implement List Zipping/solution.cpp
@@ -1,3 +1,15 @@
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+/*
+1 -> 2 -> 3 -> x
+
+3 -> 2 -> 1 -> x
+*/
+
+/*
 list<int> zipList(list<int> inList) {
 	list<int> outList;
 	for (auto i = inList.begin(); i < (inList.begin() + inList.size()) / 2; ++i) {
@@ -10,4 +22,63 @@ list<int> zipList(list<int> inList) {
 	} 
 
 	return outList;
+}
+*/
+
+struct Node {
+	int data;
+	Node *next;
+
+	Node() {};
+	Node(int d, Node *n) : data(d), next(n) {};
+};
+
+Node* reverseLinkedListIter(Node *head) {
+	Node *prev = nullptr, *cur = head;
+
+	while (cur != nullptr) {
+		Node* curNext = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = curNext;
+	}
+
+	return prev;
+}
+
+void recurseHelper(Node *node) {
+	if (node->next != nullptr) {
+		recurseHelper(node->next);
+		node->next->next = node;
+	}
+}
+
+void reverseLinkedListRecurse(Node *head) {
+	recurseHelper(head);
+	head->next = nullptr;
+}
+
+
+int main() {
+	Node a, b, c;
+	a = Node(1, &b);
+	b = Node(2, &c);
+	c = Node(3, nullptr);
+
+/*
+	Node *r = reverseLinkedListIter(&a);
+
+	while (r != nullptr) {
+		cout << r->data << endl;
+		r = r->next;
+	}
+*/
+
+	reverseLinkedListRecurse(&a);
+	Node *r = &c;
+
+	while (r != nullptr) {
+		cout << r->data << endl;
+		r = r->next;
+	}
 }
