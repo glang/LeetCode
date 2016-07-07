f810e25e656c56cdef82594fe544bd9bc64d3a8c
Granger Lang <grangerlang8@hotmail.com>
2016-07-07 01:02:37 -0700

    wrote iterative solution

--- Elements of Programming Interviews/Compute x^y/solution.cpp
+++ Elements of Programming Interviews/Compute x^y/solution.cpp
@@ -19,7 +19,23 @@ double exp(double x, int y) {
 	}
 }
 
+double expIter(double x, int y) {
+	double result = 1;
+
+	while (y) {
+		if (y & 1) {
+			result *= x;
+		}
+
+		result *= result;
+		y >>= 1;
+	}
+
+	return result;
+}
+
 int main() {
 	cout << exp(2, 7) << endl;
+	cout << expIter(2, 7) << endl;
 	return 0;
 }
