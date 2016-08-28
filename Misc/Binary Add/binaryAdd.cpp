15804b740ca29e868fd7765f18502fd8bd5f8894
Granger Lang <grangerlang8@hotmail.com>
2016-08-27 23:16:04 -0700

    binary add

--- Misc/Binary Add/binaryAdd.cpp
+++ Misc/Binary Add/binaryAdd.cpp
@@ -5,40 +5,23 @@
     1
 */
 
+#include <iostream>
+
+using namespace std;
+
 unsigned binaryAdd(unsigned A, unsigned B) {
 	unsigned Acpy = A, Bcpy = B, res = 0, k = 1, carry = 0;
 
 	while (Acpy || Bcpy || carry) {
-		short Abit = A | k, Bbit = B | k;
-		res |= A ^ B ^ carry;
-		carry = (A ^ B) | (A ^ k) | (B ^ k);
-		Acpy >>= 1, Bcpy >>= 1, k <<= 1;		
+		unsigned Abit = A & k, Bbit = B & k;
+		res |= Abit ^ Bbit ^ carry;
+		carry = (Abit & Bbit) | (Abit & carry) | (Bbit & carry);
+		Acpy >>= 1, Bcpy >>= 1, k <<= 1, carry <<= 1;		
 	}
 
 	return res;
 }
 
-
-
-
-
-
-
-
-unsigned binary addition(unsigned A, unsigned B) {
-	unsigned res = 0;
-	unsigned mobile = 1, A_cpy = A, B_cpy = B;
-	short carry = 0;
-
-	while (A_cpy || B_cpy || carry) {
-		unsigned Abit = A & mobile, Bbit = B & mobile;
-		res |= Abit ^ Bbit ^ carry;
-		carry = (Abit & Bbit) | (carry & (Abit | Bbit));
-		carry <<= 1;
-		A_cpy >>= 1;
-		B_cpy >>= 1;
-		mobile <<= 1;		
-	}
-
-	return res;
+int main() {
+	cout << binaryAdd(3, 3) << endl;
 }
