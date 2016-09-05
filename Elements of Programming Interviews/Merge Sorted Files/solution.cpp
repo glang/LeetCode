2a916f80fa6569fcc8d5e9168772667e2e6cab8f
Granger Lang <grangerlang8@hotmail.com>
2016-09-05 15:50:00 -0700

    fixed some bugs

--- Elements of Programming Interviews/Merge Sorted Files/solution.cpp
+++ Elements of Programming Interviews/Merge Sorted Files/solution.cpp
@@ -10,22 +10,22 @@ struct item {
 	bool operator>(item other) {
 		data > other.data;
 	}
-}
+};
 
 vector<int> mergeSortedArrays(vector<vector<int>> arrays) {
 	vector<int> result;
-	priority_queue<item, vector<item>, greater<item>> = heap;
+	priority_queue<item, vector<item>, greater<item>> heap;
 
 	for (int i = 0; i < arrays.size(); ++i) {
-		heap.emplace(item{array[0], i, 0});
+		heap.emplace(item{array[i][0], i, 0});
 	}
 
-	while (!head.empty()) {
+	while (!heap.empty()) {
 		item smallest = heap.top();
 		heap.pop();
 		result.push_back(smallest.data);
-		if (!heap.empty() && smallest.arrayIndex < arrays[smallest.arraysIndex].size() - 1) {
-			heap.emplace(item{arrays[arraysIndex][smallest.arrayIndex + 1], smallest.arraysIndex, smallest.arrayIndex + 1});
+		if (smallest.arrayIndex < arrays[smallest.arraysIndex].size() - 1) {
+			heap.emplace(item{arrays[smallest.arraysIndex][smallest.arrayIndex + 1], smallest.arraysIndex, smallest.arrayIndex + 1});
 		}
 	}
 
