955b1d0dec3d62868c90f7128db6b0f19f85e2a2
Granger Lang <grangerlang8@hotmail.com>
2016-10-02 11:23:58 -0700

    fast solution

--- LeetCode/Nth Digit/solution.cpp
+++ LeetCode/Nth Digit/solution.cpp
@@ -1,8 +1,8 @@
 /*
-0     0      9*0
-9     9      9*1
-99    90    90*2
-999   900  900*3
+0     0      9*0        0
+9     9      9*1        9
+99    90    90*2      189 
+999   900  900*3     2889
 */
 
 class Solution {
@@ -16,10 +16,14 @@ public:
             ++currentDigits;
             counter *= 10;
         }
-        
-        num = max(1ull, num);
-        for (totalDigits = max(1ull, totalDigits); totalDigits < n; totalDigits+=currentDigits) {
+
+        unsigned long long numberDifference = ceil((n - totalDigits) / currentDigits);
+        num += numberDifference;
+        totalDigits += numberDifference * currentDigits;
+
+        if ((n - totalDigits) % currentDigits != 0) {
             ++num;
+            totalDigits += currentDigits;
         }
         
         return to_string(num)[currentDigits - (totalDigits - n) - 1] - '0';
