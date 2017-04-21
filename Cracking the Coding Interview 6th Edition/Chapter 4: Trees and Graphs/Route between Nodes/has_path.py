111aa18958e0a355dcb34b87e93f8012386c4b4b
Granger Lang <grangerlang8@hotmail.com>
2017-04-20 23:53:56 -0700

    py route between nodes DFS

--- Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Route between Nodes/has_path.py
+++ Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Route between Nodes/has_path.py
@@ -12,7 +12,7 @@ def hasPathBFS(n1, n2):
 
 	while q.qsize() > 0:
 		cur = q.get()
-
+		cur.visited = True
 		for x in cur.neighbors:
 			if x is n2:
 				return True
@@ -21,8 +21,30 @@ def hasPathBFS(n1, n2):
 
 	return False
 
+def hasPathDFS(n1, n2):
+	if n1 is n2:
+		return True
+	if not n1.visited:
+		n1.visited = True
+		for x in n1.neighbors:
+			if hasPathDFS(x, n2):
+				return True
+	return False
+
+def reset(l):
+	for x in l:
+		x.visited = False
 n1, n2, n3 = GraphNode(), GraphNode(), GraphNode()
+nodes = [n1, n2, n3]
 n1.neighbors.add(n2)
 print(hasPathBFS(n1, n2))
+reset(nodes)
 print(hasPathBFS(n1, n3))
+reset(nodes)
 print(hasPathBFS(n2, n3))
+reset(nodes)
+print(hasPathDFS(n1, n2))
+reset(nodes)
+print(hasPathDFS(n1, n3))
+reset(nodes)
+print(hasPathDFS(n2, n3))
