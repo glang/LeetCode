596091e0b0cf42aa9835f4fde5593bbf32b48269
Granger Lang <grangerlang8@hotmail.com>
2017-04-26 23:13:44 -0700

    py validate BST in order method

---
+++ Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Validate BST/valid_BST.py
@@ -0,0 +1,14 @@
+def valid_BST_in_order(root):
+	last_val = None
+	return helper(root)
+
+def helper(node):
+	if node is None:
+		return True
+	if not helper(node.left):
+		return false
+	if last_val is not None and last_val > node.data:
+		return false
+	if not helper(node.right):
+		return false
+	return True
