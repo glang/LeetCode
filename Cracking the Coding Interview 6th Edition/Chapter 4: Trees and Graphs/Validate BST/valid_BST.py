818fb38c3858da3b97c91c5ad2fd9d69b7bdf36e
Granger Lang <grangerlang8@hotmail.com>
2017-04-27 01:19:08 -0700

    py validate BST check parents and cur children method

--- Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Validate BST/valid_BST.py
+++ Cracking the Coding Interview 6th Edition/Chapter 4: Trees and Graphs/Validate BST/valid_BST.py
@@ -6,10 +6,25 @@ def helper(node):
 	if node is None:
 		return True
 	if not helper(node.left):
-		return false
+		return False
 	if last_val is not None and last_val > node.data:
-		return false
+		return False
 	last_val = node.data
 	if not helper(node.right):
-		return false
+		return False
 	return True
+
+def valid_BST_other(parent, cur):
+	if cur is None:
+		return True
+	if cur.left is not None:
+		if cur.left.data > cur.data:
+			return False
+		if parent is not None and cur is parent.right and cur.left.data < parent.data:
+			return False
+	if cur.right is not None:
+		if cur.right.data < cur.data:
+			return False
+		if parent is not None and cur is parent.left and cur.right.data > parent.data:
+			return False
+	return valid_BST_other(cur, cur.left) and valid_BST_other(cur, cur.right)
